41deb41e418863637aaa4955293c975cb9ddccbf
Relich Michael <MRelich@delinfo.cz>
2023-08-24 13:35:24 +0200

    Init - srandička

--- src/BlazorApp1/BlazorApp1/Program.cs
+++ src/BlazorApp1/BlazorApp1/Program.cs
@@ -11,6 +11,8 @@ builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
 
+// X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
